1abc8a4337dd1497b2c94a107c177f29e6c9b052
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-18 20:03:26 +0200

    Create due funzioni. index che mi prende tutti i dati della tabella trains e show chhe mi prende solo l'argomento $id.

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Models\Guest\Train;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+
+
+    public function index(){
+
+        $trains = Train::all();
+
+        return view('guest.pages.trains.index', compact('trains'));
+    }
+
+    public function show($id){
+
+        $train = Train::findOrfail($id);
+
+        return view('guest.pages.trains.show', compact('train'));
+    }
 }
