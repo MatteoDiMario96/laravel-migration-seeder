f89d8a32405128c812e1c2697985299452e060e1
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-19 16:13:08 +0200

    Collegato alla rotta il controller FantasyTrain rinominato per GuestFantasyController. Dato un nome.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\Guest\PageController as GuestPageController;
 use App\Http\Controllers\Guest\TrainController as GuestTrainController;
+use App\Http\Controllers\Guest\FantasyTrainController as GuestFantasyTrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,8 @@ Route::get('/', [GuestPageController::class, 'view'])->name('guest.home');
 Route::get('/trains', [GuestTrainController::class, 'index'])->name('guest.trains.index');
 
 Route::get('/trains/{id}', [GuestTrainController::class, 'show'])->name('guest.train.show');
+
+Route::get('/fantasy-trains', [GuestFantasyTrainController::class, 'index'])->name('guest.fantasy-trains.index');
+
+Route::get('/fantasy-trains/{id}', [GuestFantasyTrainController::class, 'show'])->name('guest.fantasy-train.show');
 
